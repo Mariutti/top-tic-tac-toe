4ab2d62dddc427a4b173f697e5e7ce817cfd287b
Mariutti <pedromariutti@gmail.com>
2024-01-04 15:57:07 -0300

    feature: function to control the game flow

--- script.js
+++ script.js
@@ -1,14 +1,18 @@
 const GameBoard = (function () {
 	let gameBoard = [];
-	for (let i = 0; i < 3; i++) {
-		for (let j = 0; j < 3; j++) {
-			gameBoard.push([i, j]);
-		}
+	for (let i = 0; i < 9; i++) {
+		gameBoard.push(i);
 	}
 
-	const gameMove = () => {
-		return "moved";
-	};
+	function gameMove(i) {
+		if (gameBoard.indexOf(i) != -1) {
+			const string = `Moved square- [${i}]`;
+			gameBoard.splice(gameBoard.indexOf(i), 1);
+			return string;
+		} else {
+			return `Cannot mark square ${i}`;
+		}
+	}
 
 	return {
 		gameBoard,
@@ -17,7 +21,6 @@ const GameBoard = (function () {
 })();
 
 function createPlayer(mark) {
-	
 	let points = 0;
 
 	const getPoints = () => points;
@@ -26,42 +29,60 @@ function createPlayer(mark) {
 	};
 
 	return { mark, getPoints, increasePoints };
-};
+}
 
-const gameFlow = (function(){
-	let turn;
-	
-	const ct = changeTurn(){
-		turn? !turn : turn;
+const gameFlow = (() => {
+	const pOne = createPlayer("x");
+	const pTwo = createPlayer("o");
+
+	let playerOne = `Player ${pOne.mark}`;
+	let playerTwo = `Player ${pTwo.mark}`;
+
+	let turn = playerOne;
+	function getSquare(i) {
+		GameBoard.gameMove(i);
+		console.log(`${turn} marks square ${i}`);
+		console.log("Squares left", GameBoard.gameBoard);
 	}
+	let changeTurn = () => {
+		return turn == playerOne ? (turn = playerTwo) : (turn = playerOne);
+	};
 
-	return { changeTurn }
+	let showTurn = () => {
+		return turn;
+	};
+
+	return { changeTurn, showTurn, getSquare };
 })();
 
-// const teste = GameBoard.gameBoard.splice(1,1)
-// console.log(teste);
+const gameController = (() => {
+	const game = (i) => {
+		console.log(gameFlow.showTurn());
+		gameFlow.getSquare(i);
+		gameFlow.changeTurn();
+		console.log("----------------");
+	};
 
-console.log(GameBoard);
-console.log(GameBoard.gameBoard);
-console.log(GameBoard.gameMove());
+	return { game };
+})();
+
+// console.log(GameBoard.gameBoard);
 
 const playerOne = createPlayer("x");
-playerOne.increasePoints()
-playerOne.increasePoints()
-playerOne.increasePoints()
-playerOne.increasePoints()
-playerOne.increasePoints()
-playerOne.increasePoints()
-playerOne.getPoints()
+playerOne.increasePoints();
+playerOne.increasePoints();
+playerOne.getPoints();
 
+// console.log("----------------");
 
-console.log("----------------");
+// console.log(GameBoard.gameBoard);
+// console.log("----------------");
 
+// console.log(gameFlow.showTurn());
+// console.log(gameFlow.getSquare(4));
+// console.log(GameBoard.gameBoard);
+// gameFlow.changeTurn();
+// console.log("----------------");
 
-console.log(playerOne);
-
-gameFlow
-console.log(gameFlow);
-gameFlow
-console.log(gameFlow);
-
+gameController.game(1);
+gameController.game(2);
