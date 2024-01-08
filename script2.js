244adfc90f54ec7567698fb2aa42b10e0cd66e8d
Mariutti <pedromariutti@gmail.com>
2024-01-08 16:04:20 -0300

    2nd version of script - script2

---
+++ script2.js
@@ -0,0 +1,16 @@
+
+console.log("One or Two players?");
+let numbOfPlayers = function (num){
+    if(num === 1){
+        console.log('Just one player against the IA');
+        console.log("Choose your mark");
+        return num
+    }
+    console.log('Ok, It\' a VS game');
+    console.log("Player One, choose your mark");
+}
+
+
+const gameFlow = (() => {
+
+})();
