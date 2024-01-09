f687016d0cf9a5a33601fb8c0ac28ed386d137ff
Mariutti <pedromariutti@gmail.com>
2024-01-09 16:07:45 -0300

    mudando abordagem e reiniciando algoritmo

--- script2.js
+++ script2.js
@@ -1,16 +1,64 @@
+console.log("selecione o número de players");
+const startButton = document.querySelector("#startButton");
 
-console.log("One or Two players?");
-let numbOfPlayers = function (num){
-    if(num === 1){
-        console.log('Just one player against the IA');
-        console.log("Choose your mark");
-        return num
-    }
-    console.log('Ok, It\' a VS game');
-    console.log("Player One, choose your mark");
-}
+startButton.addEventListener("click", () => {
+	// console.log("test");
+	playersNumb();
+});
 
+const playersNumb = function () {
+	let playerNumb = prompt("How many player at this game?");
 
-const gameFlow = (() => {
+	if (playerNumb) {
+		if (playerNumb.toLowerCase() === ("1" || "one")) {
+			console.log("One player");
+		} else if (playerNumb.toLowerCase() === ("2" || "two")) {
+			console.log("Two players");
+		} else {
+			console.log("Please, choose 1 or 2.");
+			playersNumb();
+		}
+	} else {
+		console.log("jogador desistiu");
+	}
+};
 
-})();
+console.log("");
+
+console.log("jogador 1 seleciona a marca (x ou o)");
+console.log("jogador 2 fica com marca que sobrar");
+
+console.log("");
+
+console.log("jogo gera o tabuleiro");
+
+console.log("");
+
+console.log("loop de jogadas");
+console.log("jogo verifica quais as células estão disponíveis");
+
+console.log("");
+
+console.log("jogador 01 faz jogada");
+console.log("jogo salva jogada/célula marcada");
+console.log("jogo verifica de jogador 01 ganhou");
+
+console.log("");
+
+console.log("jogo muda a vez para jogador 2");
+
+console.log("");
+
+console.log("jogador 02 faz jogada");
+console.log("jogo salva jogada/célula marcada");
+console.log("jogo verifica de jogador 02 ganhou");
+
+console.log("");
+
+console.log("jogo muda a vez para jogador 1");
+
+console.log(
+	"jogo repete os passos no loop de jogada até alguém vencer ou haver empate"
+);
+
+console.log("");
