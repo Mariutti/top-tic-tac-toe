0d96a0ea8110d33c97f8c5ffde538d4a346ee6a4
Mariutti <pedromariutti@gmail.com>
2024-01-10 16:09:37 -0300

    feature: implemment game flow

--- script2.js
+++ script2.js
@@ -1,64 +1,130 @@
-console.log("selecione o número de players");
+console.log("olá mundo");
 const startButton = document.querySelector("#startButton");
 
 startButton.addEventListener("click", () => {
-	// console.log("test");
-	playersNumb();
+	gameFlow();
 });
 
-const playersNumb = function () {
+function chooseNumOfPlayers() {
 	let playerNumb = prompt("How many player at this game?");
 
 	if (playerNumb) {
 		if (playerNumb.toLowerCase() === ("1" || "one")) {
 			console.log("One player");
+			return 1;
 		} else if (playerNumb.toLowerCase() === ("2" || "two")) {
 			console.log("Two players");
+			return 2;
 		} else {
 			console.log("Please, choose 1 or 2.");
-			playersNumb();
+			chooseNumOfPlayers();
 		}
 	} else {
-		console.log("jogador desistiu");
+		console.log("Jogo cancelado.");
+		return null;
 	}
-};
+}
 
-console.log("");
+function player() {
+	let mark;
+	return { mark };
+}
 
-console.log("jogador 1 seleciona a marca (x ou o)");
-console.log("jogador 2 fica com marca que sobrar");
+function chooseMarksFunc() {
+	let playerOne = player();
+	let playerTwo = player();
 
-console.log("");
+	playerOne.mark = prompt("Please, select 'x' or 'o'");
 
-console.log("jogo gera o tabuleiro");
+	if (playerOne.mark) {
+		if (
+			playerOne.mark.toLowerCase() === "x" ||
+			playerOne.mark.toLowerCase() === "o"
+		) {
+			playerOne.mark == "x" ? (playerTwo.mark = "o") : (playerTwo.mark = "x");
+			return { playerOne, playerTwo };
+		} else {
+			return chooseMarksFunc();
+		}
+	} else {
+		console.log("Jogo cancelado.");
+		return null;
+	}
+}
 
-console.log("");
+function gameFlow() {
+	console.clear();
 
-console.log("loop de jogadas");
-console.log("jogo verifica quais as células estão disponíveis");
+	// Escolhendo o número de jogadores (p vs IA ou p vs p)
+	const numOfPlayers = chooseNumOfPlayers();
+	if (numOfPlayers === null) {
+		return;
+	}
 
-console.log("");
+	// Jogador 01 escolhe a marca que usará
+	const chooseMarks = chooseMarksFunc();
 
-console.log("jogador 01 faz jogada");
-console.log("jogo salva jogada/célula marcada");
-console.log("jogo verifica de jogador 01 ganhou");
+	console.log("");
 
-console.log("");
+	console.log(`jogador 1 seleciona a marca ${chooseMarks.playerOne.mark}`);
+	console.log(`jogador 2 fica com marca ${chooseMarks.playerTwo?.mark}`);
 
-console.log("jogo muda a vez para jogador 2");
+	console.log("");
 
-console.log("");
+	// gerando o tabuleiro
+	const GameBoard = (function () {
+		console.log('gerando board');
+		let gameBoard = [];
+		for (let i = 0; i < 9; i++) {
+			gameBoard.push(i);
+		}
+	
+		function gameMove(i) {
+			if (gameBoard.indexOf(i) != -1) {
+				const string = `Moved square- [${i}]`;
+				gameBoard.splice(gameBoard.indexOf(i), 1);
+				return string;
+			} else {
+				return `Cannot mark square ${i}`;
+			}
+		}
+	
+		return {
+			gameBoard,
+			gameMove,
+		};
+	})();
 
-console.log("jogador 02 faz jogada");
-console.log("jogo salva jogada/célula marcada");
-console.log("jogo verifica de jogador 02 ganhou");
+	console.log(GameBoard.gameBoard);
 
-console.log("");
+	console.log("");
 
-console.log("jogo muda a vez para jogador 1");
+	console.log("loop de jogadas");
+	console.log("jogo verifica quais as células estão disponíveis");
 
-console.log(
-	"jogo repete os passos no loop de jogada até alguém vencer ou haver empate"
-);
+	console.log("");
 
-console.log("");
+	console.log("jogador 01 faz jogada");
+	console.log("jogo salva jogada/célula marcada");
+	console.log("jogo verifica de jogador 01 ganhou");
+
+	console.log("");
+
+	console.log("jogo muda a vez para jogador 2");
+
+	console.log("");
+
+	console.log("jogador 02 faz jogada");
+	console.log("jogo salva jogada/célula marcada");
+	console.log("jogo verifica de jogador 02 ganhou");
+
+	console.log("");
+
+	console.log("jogo muda a vez para jogador 1");
+
+	console.log(
+		"jogo repete os passos no loop de jogada até alguém vencer ou haver empate"
+	);
+
+	console.log("");
+}
