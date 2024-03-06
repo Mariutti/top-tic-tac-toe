ad3484b2433452145a7fb635ee3549ba8458c8cf
Pedro Alencar <pedromariutti@gmail.com>
2024-03-06 20:08:29 -0300

    script-style: game working with some bugs. Style to show result

--- script3.js
+++ script3.js
@@ -1,6 +1,8 @@
 console.log("olá mundo");
 const startButton = document.querySelector("#startButton");
-const arr = [];
+const tableGame = document.querySelector(".table");
+const gameOverText = document.querySelector(".gameOverText");
+let arr = [];
 let count = 0;
 let winArrays = [
 	[0, 1, 2],
@@ -13,13 +15,7 @@ let winArrays = [
 	[2, 4, 6],
 ];
 
-startButton.addEventListener("click", () => {
-	console.log(arr);
-	console.log("Player One", playerOne.getCells());
-	console.log("Player Two", playerTwo.getCells());
-	console.log("count = ", count);
-	// gamePlay();
-});
+
 
 const cells = document.querySelectorAll(".cell");
 
@@ -28,16 +24,32 @@ playerOne.setMark("x");
 let playerTwo = player();
 playerTwo.setMark("o");
 
-function markCell(mark) {	
-	const cellClass = '.'+mark
+function resetGame() {
+	arr = [];
+	cells.forEach((cell) => {
+		cell.classList.remove("x", "o");
+	});
+	playerOne.playerReset();
+	playerTwo.playerReset();
+	count = 0;
+	tableGame.style.filter = "blur()";
+	gameOverText.style.display = "none";
+	cells.forEach((cell) => {
+		cell.innerText = "";
+	});
+}
+function markCell(mark) {
+	const cellClass = "." + mark;
 	const xMark = document.querySelectorAll(cellClass);
 	xMark.forEach((el) => {
 		el.innerText = mark;
 	});
 }
 let timePlayer;
+let previousPlayer;
 if (!timePlayer) {
 	timePlayer = playerOne;
+	previousPlayer = playerTwo;
 }
 let winner = "";
 const clickCell = function (arr) {
@@ -45,13 +57,13 @@ const clickCell = function (arr) {
 	for (let i = 0; i < cells.length; i++) {
 		cells[i].addEventListener("click", () => {
 			console.log(`Player ${timePlayer.getMark()} clicked cell ${i}`);
-			if (arr.includes(i)) {
+			if (arr.includes(i) || cells[i].classList.contains(previousPlayer.getMark())) {
 				console.log(`Cell ${i} already marked`);
 				console.log("try again");
 			} else {
 				cells[i].classList.add(`${timePlayer.getMark()}`);
-				markCell(timePlayer.getMark())
-				
+				markCell(timePlayer.getMark());
+
 				arr.push(i);
 				arr.sort();
 				cell = i;
@@ -64,28 +76,43 @@ const clickCell = function (arr) {
 				) {
 					isGameOver = true;
 					winner = timePlayer.getMark();
-					// cells.forEach((cell)=>{
-					// 	cell.classList.remove('board')
-					// })
-					return showWinner();
+					showWinner();
 				}
 				if (count >= 9) {
 					console.log(`It's a draw!`);
-					// } else {
-					// 	console.log(`JOGO FINALIZADO, player ${winner} WINS`);
+					draw();
 				}
+				console.log("arr:", arr);
+				console.log("count:", count);
+				console.log("Player One", playerOne.getCells());
+				console.log("Player Two", playerTwo.getCells());
 				timePlayer === playerOne
 					? (timePlayer = playerTwo)
 					: (timePlayer = playerOne);
+				previousPlayer === playerOne
+					? (previousPlayer = playerTwo)
+					: (previousPlayer = playerOne);
 			}
 		});
 	}
+	return { resetGame };
 };
 
 function showWinner() {
+	gameOverText.innerHTML = `Player<br><span>${timePlayer.getMark()}</span><br>wins!`;
+
+	tableGame.style.filter = "blur(.4rem)";
+	gameOverText.style.display = "block";
+
 	console.log(`Player ${timePlayer.getMark()} wins!!`);
 }
 
+function draw(){
+	gameOverText.innerHTML = `It's a draw!`;
+
+	tableGame.style.filter = "blur(.4rem)";
+	gameOverText.style.display = "block";
+}
 function player() {
 	let mark;
 	let points = 0;
@@ -116,17 +143,42 @@ function player() {
 		points++;
 	}
 
-	return { getMark, setMark, getCells, setCells, getPoints, increasePoints };
+	function playerReset() {
+		cells = [];
+	}
+
+	return {
+		getMark,
+		setMark,
+		getCells,
+		setCells,
+		getPoints,
+		increasePoints,
+		playerReset,
+	};
 }
 
 let isGameOver = false;
 
-(function gamePlay() {
+function gamePlay() {
 	clickCell(arr);
+}
+gamePlay();
 
-	// if (isGameOver === false) {
-	// 	console.log(`It's a draw!`);
-	// } else {
-	// 	console.log(`JOGO FINALIZADO, player ${winner} WINS`);
-	// }
-})();
+startButton.addEventListener("click", () => {
+	resetGame();
+	arr = [];
+	cells.forEach((cell) => {
+		cell.classList.remove("x");
+		cell.classList.remove("o");
+	});
+	playerOne.playerReset();
+	playerTwo.playerReset();
+	count = 0;
+	tableGame.style.filter = "blur()";
+	gameOverText.style.display = "none";
+	cells.forEach((cell) => {
+		cell.innerText = "";
+	});
+	gamePlay();
+});
